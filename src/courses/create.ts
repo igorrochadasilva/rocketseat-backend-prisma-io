8621c29b15f62ecfb0be181c1fe63dd65ca60001
Igor Silva <igor.silva@performait.com>
2022-12-27 11:51:54 -0300

    aula14 start

--- src/courses/create.ts
+++ src/courses/create.ts
@@ -3,25 +3,15 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const reuslt = await prisma.courses.create({
+  const result = await prisma.courses.create({
     data: {
-      name: "Curso de Java",
-      duration: 500,
-      description: "Curso de Java 17",
-      teacher: {
-        connectOrCreate: {
-          where: {
-            name: "Daniele Leão Evangelista",
-          },
-          create: {
-            name: "Daniele Leão Evangelista",
-          },
-        },
-      },
+      duration: 300,
+      name: "Curso de React Native",
+      description: "Curso excelente de React Native com Rodrigão",
     },
   });
 
-  console.log(reuslt);
+  console.log(result);
 }
 
 main();
